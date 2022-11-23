818108318c245ea504bb0716cf4d97628834c2e7
Rilter <rilter.tn@gmail.com>
2022-11-23 09:40:19 -0500

    testes

--- final_project/machinetranslation/tests.py
+++ final_project/machinetranslation/tests.py
@@ -4,12 +4,16 @@ from translator import englishToFrench, frenchToEnglish
 
 class TestEnglishToFrench(unittest.TestCase): 
     def test_null(self): 
-        self.assertIsNone(englishToFrench(None))
+        self.assertRaises(ValueError, englishToFrench(None))
+    def test_translation(self): 
+        self.assertEqual(englishToFrench('Hello'),'Bonjour')
         
 
 class TestFrenchToEnglish(unittest.TestCase): 
     def test_null(self): 
-        self.assertIsNone(englishToFrench(None))
+        self.assertRaises(ValueError, frenchToEnglish(None))
+    def test_translation(self): 
+        self.assertEqual(frenchToEnglish('Bonjour'),'Hello')
         
         
 unittest.main()
